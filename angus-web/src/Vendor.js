8870504022f2f2b544ac33ea665100c80d07bd49
Renaud Dumont <renaud.dumont@live.fr>
2018-08-03 11:00:03 +0200

    added web react bootstrap

---
+++ angus-web/src/Vendor.js
@@ -0,0 +1,10 @@
+// Modernizr
+import './modernizr.js';
+// Whirl
+import 'whirl/dist/whirl.css';
+// Font Awesome
+import 'font-awesome/css/font-awesome.min.css';
+// Animate.CSS
+import 'animate.css/animate.min.css';
+// Simple line icons
+import 'simple-line-icons/css/simple-line-icons.css';
