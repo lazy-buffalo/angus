7ee4d5bfeb5ce747b7fda76dbc78b6ceacdfabb1
kRapaille <kevin.rapaille@hotmail.com>
2018-08-04 10:49:44 +0200

    Fake locations

--- LazyBuffalo.Angus/LazyBuffalo.Angus.Api/Controllers/CowController.cs
+++ LazyBuffalo.Angus/LazyBuffalo.Angus.Api/Controllers/CowController.cs
@@ -2,6 +2,8 @@
 using LazyBuffalo.Angus.Api.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -43,6 +45,65 @@ namespace LazyBuffalo.Angus.Api.Controllers
                            + (double)cowLocation.GpsEntry.LongitudeMinutes / 60
                            + (double)cowLocation.GpsEntry.LongitudeSecondes / 3600
             });
+
+            return new JsonResult(result);
+        }
+
+        // GET api/cows/locations/fake
+        [HttpGet("locations/fake")]
+        public IActionResult GetFakeLocations()
+        {
+            var result = new List<CowLocationDto>
+            {
+                new CowLocationDto
+                {
+                    CowId = 1,
+                    CowName = "Roberte",
+                    LocationDateTime = DateTime.UtcNow,
+                    Latitude = 50.60192548443745,
+                    Longitude = 3.51157760612557
+                },
+                new CowLocationDto
+                {
+                    CowId = 2,
+                    CowName = "Marguerite",
+                    LocationDateTime = DateTime.UtcNow,
+                    Latitude = 50.601907041658286,
+                    Longitude = 3.5116938560830704
+                },
+                new CowLocationDto
+                {
+                    CowId = 3,
+                    CowName = "Kiri",
+                    LocationDateTime = DateTime.UtcNow,
+                    Latitude = 50.60185369942146,
+                    Longitude = 3.511409115792844
+                },
+                new CowLocationDto
+                {
+                    CowId = 4,
+                    CowName = "Amandine",
+                    LocationDateTime = DateTime.UtcNow,
+                    Latitude = 50.60186164401623,
+                    Longitude = 3.511526706854852
+                },
+                new CowLocationDto
+                {
+                    CowId = 5,
+                    CowName = "Marcele",
+                    LocationDateTime = DateTime.UtcNow,
+                    Latitude = 50.60189342237546,
+                    Longitude = 3.511735493023366
+                },
+                new CowLocationDto
+                {
+                    CowId = 6,
+                    CowName = "Zelda",
+                    LocationDateTime = DateTime.UtcNow,
+                    Latitude = 50.601936558444415,
+                    Longitude = 3.51033373468681
+                }
+            };
 
             return new JsonResult(result);
         }
