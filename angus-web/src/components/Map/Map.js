3680a21a2087cd548130380087343fcbd2dbe624
Renaud Dumont <renaud.dumont@live.fr>
2018-08-04 18:56:19 +0200

    fixed calendar

--- angus-web/src/components/Map/Map.js
+++ angus-web/src/components/Map/Map.js
@@ -1,22 +1,38 @@
 import React, {Component} from 'react';
 import {withGoogleMap, GoogleMap} from 'react-google-maps';
 
-const MapRendering = withGoogleMap((props) => <GoogleMap
-  defaultCenter={{lat: 50.601878, lng: 3.511215}}
-  defaultZoom={18}
-  defaultMapTypeId="satellite"
->
-  {props.children}
-</GoogleMap>);
+const MapRendering = withGoogleMap((props) => {
+
+  const onRef = (e) => {
+    if (props.ref) {
+      props.ref(e);
+    }
+  };
+
+  return (<GoogleMap
+    defaultCenter={{lat: 50.601878, lng: 3.511215}}
+    defaultZoom={18}
+    defaultMapTypeId="satellite"
+    ref={props.onMapMounted}
+  >
+    {props.children}
+  </GoogleMap>)
+});
 
 class Map extends Component {
   constructor(props) {
     super(props);
   }
 
+  handleMapMounted = (map) => {
+    this._map = map;
+    this.props.mapRef(map);
+  };
+
   render() {
     return <MapRendering containerElement={<div style={this.props.style}/>}
-                         mapElement={<div style={{height: `100%`}}/>}>
+                         mapElement={<div style={{height: `100%`}}/>}
+                         onMapMounted={this.handleMapMounted}>
       {this.props.children}
       {this.props.layer &&
       (this.props.layer)}
