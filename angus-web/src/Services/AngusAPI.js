52cf0732592cda70f957b9673c47a43d8aa759ea
kRapaille <kevin.rapaille@hotmail.com>
2018-08-04 18:34:53 +0200

    Delete gps entries endpoint url fix

--- angus-web/src/Services/AngusAPI.js
+++ angus-web/src/Services/AngusAPI.js
@@ -17,7 +17,7 @@ const getCows = (start, end) => {
 };
 
 const deleteLocations = () => {
-  return fetch(`${baseUrl}/api/delete/locations`, {
+  return fetch(`${baseUrl}/api/cows/delete/locations`, {
     method: "POST"
   })
 };
