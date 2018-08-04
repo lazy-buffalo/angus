3ad7ceef239572a7dbe4a81ea1f6ed734e85b425
kRapaille <kevin.rapaille@hotmail.com>
2018-08-04 12:38:25 +0200

    Genrate fake locations

--- LazyBuffalo.Angus/LazyBuffalo.Angus.Api/Controllers/CowController.cs
+++ LazyBuffalo.Angus/LazyBuffalo.Angus.Api/Controllers/CowController.cs
@@ -52,84 +52,27 @@ namespace LazyBuffalo.Angus.Api.Controllers
             return new JsonResult(result);
         }
 
-        [HttpGet("locations/last/fake/{cowId?}")]
-        public IActionResult GetFakeLastLocations(int? cowId)
+        [HttpGet("locations/last/fake/{numberOfCows}/{cowId?}")]
+        public IActionResult GetFakeLastLocations(int numberOfCows, int? cowId)
         {
-            var result = new List<CowLastLocationDto>
+            var ids = new List<int>();
+            for (var i = 0; i < numberOfCows; i++)
             {
-                new CowLastLocationDto
+                ids.Add(i + 1);
+            }
+
+            var result = ids.Select(id => new CowLastLocationDto
+            {
+                CowId = id,
+                CowName = "Roberte",
+                Location = new LocationDto
                 {
-                    CowId = 1,
-                    CowName = "Roberte",
-                    Location = new LocationDto
-                    {
-                        Id = 1,
-                        LocationDateTime = DateTime.UtcNow.ToLocalTime(),
-                        Latitude = 50.60192548443745,
-                        Longitude = 3.51157760612557
-                    }
-                },
-                new CowLastLocationDto
-                {
-                    CowId = 2,
-                    CowName = "Marguerite",
-                    Location = new LocationDto
-                    {
-                        Id = 2,
-                        LocationDateTime = DateTime.UtcNow.ToLocalTime(),
-                        Latitude = 50.601907041658286,
-                        Longitude = 3.5116938560830704
-                    }
-                },
-                new CowLastLocationDto
-                {
-                    CowId = 3,
-                    CowName = "Kiri",
-                    Location = new LocationDto
-                    {
-                        Id = 3,
-                        LocationDateTime = DateTime.UtcNow.ToLocalTime(),
-                        Latitude = 50.60185369942146,
-                        Longitude = 3.511409115792844
-                    }
-                },
-                new CowLastLocationDto
-                {
-                    CowId = 4,
-                    CowName = "Amandine",
-                    Location = new LocationDto
-                    {
-                        Id = 4,
-                        LocationDateTime = DateTime.UtcNow.ToLocalTime(),
-                        Latitude = 50.60186164401623,
-                        Longitude = 3.511526706854852
-                    }
-                },
-                new CowLastLocationDto
-                {
-                    CowId = 5,
-                    CowName = "Marcele",
-                    Location = new LocationDto
-                    {
-                        Id = 5,
-                        LocationDateTime = DateTime.UtcNow.ToLocalTime(),
-                        Latitude = 50.60189342237546,
-                        Longitude = 3.511735493023366
-                    }
-                },
-                new CowLastLocationDto
-                {
-                    CowId = 6,
-                    CowName = "Zelda",
-                    Location = new LocationDto
-                    {
-                        Id = 6,
-                        LocationDateTime = DateTime.UtcNow.ToLocalTime(),
-                        Latitude = 50.601936558444415,
-                        Longitude = 3.51033373468681
-                    }
+                    Id = id,
+                    LocationDateTime = DateTime.UtcNow.ToLocalTime(),
+                    Latitude = GetRandomLatitude(),
+                    Longitude = GetRandomLongitude()
                 }
-            };
+            });
 
             if (cowId.HasValue)
             {
@@ -170,6 +113,67 @@ namespace LazyBuffalo.Angus.Api.Controllers
             });
 
             return new JsonResult(result);
+        }
+
+
+        [HttpGet("locations/fake/{numberOfCows}/{numberOfEntries}/{cowId?}")]
+        public IActionResult GetFakeLocations(int numberOfCows, int numberOfEntries, int? cowId)
+        {
+            var cowIds = new List<int>();
+            for (var i = 0; i < numberOfCows; i++)
+            {
+                cowIds.Add(i + 1);
+            }
+
+            var entryIds = new List<int>();
+            for (var i = 0; i < numberOfEntries; i++)
+            {
+                entryIds.Add(i + 1);
+            }
+
+            var result = cowIds.Select(id => new CowLocationDto
+            {
+                CowId = id,
+                CowName = "Roberte",
+                Locations = entryIds.Select(e => new LocationDto
+                {
+                    Id = Convert.ToInt64(id + e.ToString()),
+                    LocationDateTime = DateTime.UtcNow.ToLocalTime(),
+                    Latitude = GetRandomLatitude(),
+                    Longitude = GetRandomLongitude()
+                }).ToList()
+            });
+
+            if (cowId.HasValue)
+            {
+                result = result.Where(x => x.CowId == cowId.Value).ToList();
+            }
+
+            return new JsonResult(result);
+        }
+
+
+        private static double GetRandomLatitude()
+        {
+            const int minRange = 35103;
+            const int maxRange = 35117;
+
+            return GetRandomCoordinate(minRange, maxRange, 1000);
+        }
+
+        private static double GetRandomLongitude()
+        {
+            const int minRange = 506017;
+            const int maxRange = 506018;
+
+            return GetRandomCoordinate(minRange, maxRange, 10000);
+        }
+
+        private static double GetRandomCoordinate(double minNumber, double maxNumber, int divider)
+        {
+            var result = new Random().NextDouble() * (minNumber - maxNumber) + minNumber;
+
+            return result / divider;
         }
     }
 }
