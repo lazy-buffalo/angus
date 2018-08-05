0dfe77e23c1001f40ee03bd364607472f31ecb2d
Renaud Dumont <renaud.dumont@live.fr>
2018-08-05 13:19:52 +0200

    simplified layout

--- angus-web/src/components/Layout/Base.js
+++ angus-web/src/components/Layout/Base.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Header from './Header'
 import Sidebar from './Sidebar'
-import Offsidebar from './Offsidebar'
 import Footer from './Footer'
 
 const Base = props => (
@@ -10,8 +9,6 @@ const Base = props => (
         <Header />
 
         <Sidebar />
-
-        <Offsidebar />
 
         <section className="section-container">
             { props.children }
--- angus-web/src/components/Layout/BasePage.js
+++ angus-web/src/components/Layout/BasePage.js
@@ -4,6 +4,6 @@ const BasePage = props =>(
     <div className="wrapper">
         { props.children }
     </div>
-)
+);
 
 export default BasePage;
